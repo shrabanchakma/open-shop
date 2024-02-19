d47ba303133eb2381d6e52d2de9b09a47c646d9a
Shraban Chakma <shrabanchakma328@gmail.com>
2024-02-19 23:39:42 +0600

    added images and sliding functionality using buttons

--- src/Components/pages/Home/Slider/Slider.jsx
+++ src/Components/pages/Home/Slider/Slider.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-
+import { FcPrevious, FcNext } from "react-icons/fc";
 const Slider = ({ page, limit }) => {
   const [images, setImages] = useState([]);
+  const [currentImg, setCurrentImg] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
   async function getImages() {
     try {
       const res = await fetch(
@@ -10,12 +12,25 @@ const Slider = ({ page, limit }) => {
       );
       const data = await res.json();
       console.log(data);
+      setImages(data);
       setLoading(false);
     } catch (error) {
-      console.error(error);
+      setErrorMsg(error.message);
       setLoading(false);
     }
   }
+
+  function goLeft(currentIdx) {
+    currentIdx === 0
+      ? setCurrentImg(images.length - 1)
+      : setCurrentImg(currentImg - 1);
+  }
+  function goRight(currentIdx) {
+    currentIdx === images.length - 1
+      ? setCurrentImg(0)
+      : setCurrentImg(currentIdx + 1);
+  }
+
   useEffect(() => {
     getImages();
   }, [page, limit]);
@@ -27,8 +42,51 @@ const Slider = ({ page, limit }) => {
       </div>
     );
 
+  if (errorMsg)
+    return (
+      <div className="flex justify-center items-center h-[calc(100vh-64px)]">
+        {errorMsg}
+      </div>
+    );
+
   return (
-    <div className="h-[calc(100vh-64px)] bg-yellow-500">this is a slider</div>
+    <>
+      <div className="h-[calc(100vh-64px)] bg-yellow-500 flex justify-center  w-full relative">
+        <button
+          onClick={() => goLeft(currentImg)}
+          className="text-4xl absolute bottom-0 right-[47px] bg-base-200 rounded-sm py-[.4rem] px-[.3rem]"
+        >
+          <FcPrevious />
+        </button>
+        {images && images.length ? (
+          images.map((imageItem, idx) => (
+            <img
+              key={imageItem.id}
+              src={imageItem.download_url}
+              alt={imageItem.download_url}
+              className={
+                currentImg === idx ? "block w-full object-cover" : "hidden"
+              }
+            />
+          ))
+        ) : (
+          <div>no images available</div>
+        )}
+        <button
+          onClick={() => goRight(currentImg)}
+          className="text-4xl absolute bottom-0 right-0 bg-base-200 rounded-sm py-[.4rem] px-[.3rem]"
+        >
+          <FcNext />
+        </button>
+      </div>
+      {/* <span>
+        {images && images.length ? (
+          images.map((_, idx) => <button key={idx} className="h-15px w-15px"></button>)
+        ) : (
+          <div>no images available</div>
+        )}
+      </span> */}
+    </>
   );
 };
 
